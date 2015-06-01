c3f8431967b3ee810b4770d8466a23684c28486c
Stephane Alnet <stephane@shimaore.net>
2015-06-01 15:43:08 +0200

    `run.sh` should be a little bit more than the list of commands to run at the prompt

--- example/run.sh
+++ example/run.sh
@@ -5,10 +5,11 @@ PKG=`jq -r .name package.json`
 npm install
 
 # Start the Event Socket server
-./node_modules/bin/coffee index.coffee.md
+./node_modules/bin/coffee index.coffee.md &
 # Start FreeSwitch
 docker run --rm -t -i --net host \
   --name $PKG-run \
   -v ${PWD}/conf:/opt/freeswitch/conf \
   shimaore/freeswitch \
   /opt/freeswitch/bin/freeswitch -nf -nosql -nonat -nonatmap -nocal -nort -c
+# Type `shutdown` in the FreeSwitch console to stop it.
